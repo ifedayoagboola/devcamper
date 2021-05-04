6baa7f28f13ae7e81df6fc1adb4e5576e7667b29
ifedayo <ifedayoagboola19@gmail.com>
2021-05-04 11:02:37 +0100

    First major push

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to DB
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
 
 //Load config file
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+//Dev loggin middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
